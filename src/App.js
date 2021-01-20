5236e1085f14ba4452e480122184b54c3413c356
lukedam <lukedam@gmail.com>
2021-01-20 18:18:50 -0500

    local

--- src/App.js
+++ src/App.js
@@ -22,44 +22,9 @@ const GET_POKEMON_INFO = gql`
 `;
 
 function App() {
-  const { data, loading, error } = useQuery(GET_POKEMON_INFO);
-
-  if (loading) return <p>Loading...</p>;
-
-  if (error) return <p>Error....</p>;
-
-  return (
-    <>
-      <h1>Pokemons</h1>
-      <p>
-        <a href="https://en.wikipedia.org/wiki/List_of_Pok%C3%A9mon">Wiki</a>
-        Catch 'em all
-      </p>
-      <div className="container">
-        {data &&
-          data.pokemons &&
-          data.pokemons.map((pokemon, index) => (
-            <div key={index} className="card">
-              <img src={pokemon.image} />
-              <div class="card-body">
-                <h3>{pokemon.name}</h3>
-                <p>
-                  {pokemon.evolutions && pokemon.evolutions.length !== 0 && (
-                    <p>
-                      {" "}
-                      Evolutions:
-                      {pokemon.evolutions.map((e, indx) => {
-                        return <p key={indx}> {e.name} </p>;
-                      })}
-                    </p>
-                  )}
-                </p>
-              </div>
-            </div>
-          ))}
-      </div>
-    </>
-  );
+  <>
+    <h1>hi</h1>
+  </>
 }
 
 export default App;
